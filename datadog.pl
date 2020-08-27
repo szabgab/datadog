1f2ac3c02a504b2ccbcec522dee00a7865dfa8dc
Gabor Szabo <gabor@szabgab.com>
2020-08-27 10:58:36 +0300

    update datadog

--- datadog.pl
+++ datadog.pl
@@ -4,39 +4,43 @@ use v5.30;
 
 my ($debug) = @ARGV;
 
-eval "use Net::Dogstatsd";    ## no critic
-my $datadog = ! $@;
+use DataDog::DogStatsd;
+my $start = time;
+
+#eval "use Net::Dogstatsd";    ## no critic
+#my $datadog = ! $@;
+
 
 my @planets = ('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn');
 
-my %data = (
-    elapsed_time => rand(),
-    request      => $planets[int(rand() * scalar @planets)],
-);
-
-log_to_datadog(\%data);
-exit();
-
-
-sub log_to_datadog {
-	my ($data) = @_;
-
-	return if not $datadog;
+while (1) {
+    my $sleep = rand()/2;
+    sleep($sleep);
     if ($debug) {
-        say 'sending';
+        say "send $sleep";
     }
+    my $elapsed_time = rand();
+    my $request  = $planets[int(rand() * scalar @planets)];
 
-	my $dogstatsd = Net::Dogstatsd->new();
-	my $socket    = $dogstatsd->get_socket();
-	$dogstatsd->increment( name => 'web.page_views', );
-	$dogstatsd->gauge(
-		name  => 'web.elapsed_time',
-		value => $data->{elapsed_time},
-	);
-	$dogstatsd->histogram(
-		name  => 'web.elapsed_time_histogram',
-		value => $data->{elapsed_time},
-	);
+    my $statsd = DataDog::DogStatsd->new;
+    $statsd->increment("demo_perl.$request");
+    $statsd->increment("demo_perl.requests");
+    $statsd->gauge('demo_perl.elapsed_time', $elapsed_time);
+
+
+    #my $dogstatsd = Net::Dogstatsd->new();
+    #my $socket    = $dogstatsd->get_socket();
+    #$dogstatsd->increment( name => 'web.page_views', );
+    #$dogstatsd->gauge(
+    #	name  => 'web.elapsed_time',
+    #	value => $data->{elapsed_time},
+    #);
+    #$dogstatsd->histogram(
+    #	name  => 'web.elapsed_time_histogram',
+    #	value => $data->{elapsed_time},
+    #);
+    #
+    #
+    last if (time - $start) >= 60;
 }
-
 
